407fe018b20a4da2528fdb3a6d2cd29ec38e7920
nisuky-devs <149247934+nisuky-devs@users.noreply.github.com>
2023-11-13 08:55:05 +0000

    update

--- web/online/nisuky-tv/login.js
+++ web/online/nisuky-tv/login.js
@@ -1,4 +1,4 @@
-document.getElementById('discord-login-button').addEventListener('click', function() {
-    // Redirect to the Discord login page
-    window.location.href = 'https://discord.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code&scope=identify';
-   });
+function redirectToDiscord() {
+    // Rediriger vers la page de connexion Discord
+    window.location.href = "URL_DE_CONNEXION_DISCORD";
+  }
