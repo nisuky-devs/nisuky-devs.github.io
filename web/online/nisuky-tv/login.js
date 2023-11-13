a392742dcacd15dbad769e0fdfa03cbd367d5a32
nisuky-devs <149247934+nisuky-devs@users.noreply.github.com>
2023-11-13 15:00:00 +0000

    adding Discord Login

--- web/online/nisuky-tv/login.js
+++ web/online/nisuky-tv/login.js
@@ -1,4 +1,4 @@
 function redirectToDiscord() {
     // Rediriger vers la page de connexion Discord
-    window.location.href = "URL_DE_CONNEXION_DISCORD";
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1167947811040546847&redirect_uri=https%3A%2F%2Fnisuky-devs.github.io%2Fweb%2Fonline%2Fnisuky-tv%2Flogged%2Floading.html&response_type=code&scope=identify%20guilds.join";
   }
