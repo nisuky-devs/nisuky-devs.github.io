6e022a1189fa83c11aacd08a4ece36500eec6053
Nakano San <antoinef30350@gmail.com>
2024-07-21 23:31:57 +0000

    yeehah

--- pop-up_loader/script.js
+++ pop-up_loader/script.js
@@ -1,1 +1,26 @@
-// d
+document.addEventListener("DOMContentLoaded", function () {
+    var popup = document.getElementById('popup');
+    var closeBtn = document.getElementsByClassName('close')[0];
+
+    // Fonction pour afficher le popup
+    function showPopup() {
+        popup.style.display = 'flex';
+        document.getElementById('update-date').innerText = new Date().toLocaleDateString();
+        document.getElementById('system-time').innerText = new Date().toLocaleTimeString();
+    }
+
+    // Afficher le popup après 3 secondes pour le test
+    setTimeout(showPopup, 3000);
+
+    // Fermer le popup quand l'utilisateur clique sur le bouton 'close'
+    closeBtn.onclick = function () {
+        popup.style.display = 'none';
+    };
+
+    // Fermer le popup si l'utilisateur clique à l'extérieur du contenu du popup
+    window.onclick = function (event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+        }
+    };
+});
